9e9cdab98a97c3fbee449ffa7e98ad7a945ca78f
Lizardo-Castillo <lcastillol@unsa.edu.pe>
2024-12-10 00:22:04 -0500

    Se agrego pruebas unitarias en ResourceNotFoundExceiptionTest

--- src/test/java/com/backend/backend/exception/ResourceNotFoundExceptionTest.java
+++ src/test/java/com/backend/backend/exception/ResourceNotFoundExceptionTest.java
@@ -1,5 +1,21 @@
 package com.backend.backend.exception;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
 public class ResourceNotFoundExceptionTest {
 
+    @Test
+    void testExceptionMessage() {
+        String errorMessage = "Resource not found";
+        ResourceNotFoundException exception = new ResourceNotFoundException(errorMessage);
+        assertThat(exception.getMessage()).isEqualTo(errorMessage);
+    }
+
+    @Test
+    void testExceptionInheritance() {
+        ResourceNotFoundException exception = new ResourceNotFoundException("Test message");
+        assertThat(exception).isInstanceOf(RuntimeException.class);
+    }
 }
